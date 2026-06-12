cdd521f5d4927a341a265ef9a158e29d3c43ef36
Aadi Jain <aadij5645@gmail.com>
2026-06-12 13:58:24 +0530

    Day-7 uploaded

---
+++ Day-7/Recursive_Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Recursive_Factorial 
+{
+    static int factorial(int n) 
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = sc.nextInt();
+        System.out.println("Factorial = " + factorial(number));
+    }
+}
